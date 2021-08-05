3f61016ada5b6385917d02aecb094f4fb1ce7c64
mosgang <mosda6353@gmail.com>
2021-08-05 14:02:08 +0800

    the end lesson

--- myApp.js
+++ myApp.js
@@ -1,5 +1,64 @@
 var express = require('express');
 var app = express();
 var bGround = require('fcc-express-bground');
+var bodyParser = require("body-parser");
 
+app.use((req, res, next) => {
+    console.log(req.method + " " + req.path + " - " + req.ip);
+    next(); 
+  });
+  app.use(bodyParser.urlencoded({extended:true}))
+  app.use(bodyParser.json());
+  
+  app.use(express.static(__dirname + "/public"));
+  
+  // Assets at the /public route
+  app.use("/public", express.static(__dirname + "/public"));
+  
+  
+  app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/views/index.html");
+  })
+  
+  app.get("/json", function (req, res) {
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+      res.json({
+        message: "Hello json".toUpperCase()
+      })
+    } else {
+      res.json({
+        message: "Hello json"
+      })
+    }
+  })
+  
+  app.get('/now', function (req, res, next) {
+    req.time = new Date().toString();
+    next();
+  }, function (req, res) {
+    res.json({
+      "time": req.time
+    })
+  });
+  
+  app.get("/:word/echo", (req, res) => {
+    const { word } = req.params;
+    res.json({
+      echo: word
+    });
+  });
+  
+  app.get("/name", function (req, res) {
+    var firstName = req.query.first;
+    var lastName = req.query.last;
+    var { first: firstName, last: lastName } = req.query;
+    res.json({
+      name: `${firstName} ${lastName}`
+    });
+  });
+  app.post("/name", function(req, res) {
+    var string = req.body.first + " " + req.body.last;
+    res.json({ name: string });
+  });
+  
  module.exports = app;
--- server.js
+++ server.js
@@ -3,7 +3,9 @@ var myApp = require("./myApp");
 var express = require("express");
 const dotenv = require('dotenv');
 const response = require("./myApp");
+const { path } = require("./myApp");
 var app = express();
+
 dotenv.config();
 if (!process.env.DISABLE_XORIGIN) {
   app.use(function (req, res, next) {
@@ -23,48 +25,8 @@ if (!process.env.DISABLE_XORIGIN) {
     next();
   });
 }
-app.use( (req , res , next) =>  {
-  console.log(req.method + " " + req.path + " - " + req.ip);
-  next();
-});
-
-
-
-app.get("/" , function(req , res) {
-  res.sendFile(__dirname + "/views/index.html");
-})
-app.use(express.static(__dirname + "/public"));
-
-// Assets at the /public route
-app.use("/public", express.static(__dirname + "/public"));
-
-app.get("/json", function(req, res){
-  if (process.env.MESSAGE_STYLE === "uppercase") {
-    res.json ({
-      message : "Hello json".toUpperCase() 
-    })
-  }else {
-    res.json ({
-      message : "Hello json" 
-    })
-  }
-})
-
-var mounted-middleware = (req, res, next) => {
-  return  new Date().toString();
-
-};
-app.get('/now' ,   function(req , res , next) {
-  req.time = middleware();
-  next();},
-  function (req , res ) {
-    res.json ({
-time : req.time
-    })
-})
 
 var port = process.env.PORT || 5000;
 bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, function () {
-  
   bGround.log("Node is listening on port " + port + "...");
 });
